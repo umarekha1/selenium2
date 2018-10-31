c9a6cb0698ab08999f16e74de862db5e3671982b
umarekha1 <rapoluumarekha@gmail.com>
2018-10-31 12:44:00 +0530

    added javaproject
    added javaprojec

---
+++ ru.java
@@ -0,0 +1,19 @@
+package javaproject;
+
+public class ru 
+{
+	
+	
+	
+	public static void main(String[] args) 
+	
+	{
+		int a=10, b=15, c=0;
+		c=a+b;
+		
+		
+		System.out.println( " add the a & b is : "+c);		
+	}
+		
+
+}
